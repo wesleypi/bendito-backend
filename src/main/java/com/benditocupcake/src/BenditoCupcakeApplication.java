65301ff010cb1fa2f656a6e211d37182ad1a5d41
wesleypi <wesleypi2@hotmail.com.br>
2025-05-20 20:43:44 -0300

    feat: add necessary files

---
+++ src/main/java/com/benditocupcake/src/BenditoCupcakeApplication.java
@@ -0,0 +1,12 @@
+package com.benditocupcake.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenditoCupcakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenditoCupcakeApplication.class, args);
+	}
+}
